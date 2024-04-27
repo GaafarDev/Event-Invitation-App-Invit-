91340cd8d81aa72311470b63baa7d97d4ba99ac7
Wiang2002 <billysoo2002@gmail.com>
2024-04-27 14:15:40 +0800

    Prototype ready

---
+++ lib/shared/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+//text
+
+const Color highlight1 = Color(0xFF00D9FF);
+const Color highlight2 = Color(0xFF2397FB);
+const Color highlight3 = Color(0xFF6BA3F7);
+const Color highlight4 = Color(0xFFB4DBF7);
+const Color highlight5 = Color(0xFFEAF2FF);
+
+const Color neutralLight1 = Color(0xFFC6C8CC);
+const Color neutralLight2 = Color(0xFFD4D8DD);
+const Color neutralLight3 = Color(0xFFE8E9F1);
+const Color neutralLight4 = Color(0xFFF8F9FE);
+const Color neutralLight5 = Color(0xFFFFFFFF);
---
+++ lib/shared/constants/sizes.dart
@@ -0,0 +1,15 @@
+const double heading1FontSize = 32;
+
+const double heading2FontSize = 24;
+
+const double heading3FontSize = 20;
+
+const double heading4FontSize = 18;
+
+const double heading5FontSize = 16;
+
+const double bodyText1FontSize = 16;
+
+const double bodyText2FontSize = 14;
+
+const double actionTextFontSize = 14;
