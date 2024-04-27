34c8cd47021821d081f69d346836579265c547ba
Wiang2002 <billysoo2002@gmail.com>
2024-04-27 18:29:40 +0800

    ProfilePageDone

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -18,46 +18,69 @@ class _ProfileScreenState extends State<ProfileScreen> {
     _future =
         FirebaseFirestore.instance.collection('users').doc(user?.uid).get();
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-      ),
-      body: FutureBuilder<DocumentSnapshot>(
-        future: _future,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            Map<String, dynamic> data =
-                snapshot.data!.data() as Map<String, dynamic>;
-            Timestamp subDateEnd = data['subDateEnd'];
-            String formattedDate =
-                DateFormat('yyyy-MM-dd').format(subDateEnd.toDate());
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Email: ${user?.email}', style: TextStyle(fontSize: 20)),
-                  Text('Full Name: ${data['fullName']}',
-                      style: TextStyle(fontSize: 20)),
-                  Text('Gender: ${data['gender']}',
-                      style: TextStyle(fontSize: 20)),
-                  Text('Phone No: ${data['phoneNo']}',
-                      style: TextStyle(fontSize: 20)),
-                  Text('Subscription End Date: $formattedDate',
-                      style: TextStyle(fontSize: 20)),
-                ],
-              ),
-            );
-          }
-        },
-      ),
-    );
-  }
-}
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('Profile'),
+    ),
+    body: FutureBuilder<DocumentSnapshot>(
+      future: _future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+          Timestamp subDateEnd = data['subDateEnd'];
+          String formattedDate =
+              DateFormat('yyyy-MM-dd').format(subDateEnd.toDate());
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ListView(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.email),
+                    title: Text('Email'),
+                    subtitle: Text('${user?.email}'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text('Full Name'),
+                    subtitle: Text('${data['fullName']}'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.person_outline),
+                    title: Text('Gender'),
+                    subtitle: Text('${data['gender']}'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.phone),
+                    title: Text('Phone No'),
+                    subtitle: Text('${data['phoneNo']}'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.date_range),
+                    title: Text('Subscription End Date'),
+                    subtitle: Text('$formattedDate'),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }
+      },
+    ),
+  );
+}}
