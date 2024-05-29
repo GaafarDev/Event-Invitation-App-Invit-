2e4c6a0b9b52af4ae3a7c48f77972864a98c193e
KhairulFahmi1412 <kaifahmi.99@gmail.com>
2024-05-29 15:23:39 +0800

    feat: Add HomePageUser route to navigation

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         // When navigating to the "/second" route, build the SecondScreen widget.
+
         '/HomePageUser': (context) => HomePage(),
       },
       debugShowCheckedModeBanner: false,
